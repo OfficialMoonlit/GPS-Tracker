5a601a6e951fddffae61006bcba7356225145c72
Moonlit <mltgit@live.co.uk>
2012-12-02 13:11:55 +0000

    Version 1.0

---
+++ track/Main.vb
@@ -0,0 +1,152 @@
+﻿Imports System.IO
+Imports System.IO.Ports
+Imports System.Threading
+
+Module Main
+
+    Public Class GPSClass
+        Public Shared GPSData As String
+    End Class
+
+    Dim CommandLineArgs As System.Collections.ObjectModel.ReadOnlyCollection(Of String) = My.Application.CommandLineArgs
+
+    Sub Main()
+        Console.WriteLine("")
+        Console.WriteLine("IcyEwe v1.0 Tracking System")
+        Console.WriteLine("")
+
+        GPSClass.GPSData = "Something"
+
+        'Thunderbirds are go.
+        Dim GPS As Thread
+        GPS = New Thread(AddressOf ProcessGPS)
+        GPS.Start()
+
+        Dim GSM As Thread
+        GSM = New Thread(AddressOf ProcessGSM)
+        GSM.Start()
+    End Sub
+
+    Private Sub BadArgs()
+        'You messed up, run it again, and this time do what it says.
+        Console.WriteLine("Error: GPS and GSM modem ports are both required.")
+        Console.WriteLine("Usage: track.exe /gps:com1 /gsm:com2")
+        Console.WriteLine("Where COM1 and COM2 are your GPS and GSM modem ports respectively.")
+        End
+    End Sub
+
+    Private Sub ProcessGPS()
+        Dim gpsPort As New SerialPort                            'New Serial Port: GPS.
+        Dim sNMEA As String
+        Dim sData As String
+
+        Try
+            With gpsPort
+                .PortName = "COM12" 'GPS port
+                .BaudRate = 4800
+                .Open()
+            End With
+            If gpsPort.IsOpen Then Console.WriteLine("GPS Online.")
+        Catch x As Exception
+            Console.WriteLine("GPS Failed. Could not access port. Check device and settings.")
+            End
+        End Try
+
+        Do
+            If gpsPort.BytesToRead > 0 And gpsPort.IsOpen = True Then
+                Try
+                    sNMEA = gpsPort.ReadLine
+                    'Console.WriteLine(sNMEA)
+                    If ParseGPS(sNMEA, "Time") <> "" Then
+                        sData = "Time: " & ParseGPS(sNMEA, "Time") & " (GMT) Lat: " & ParseGPS(sNMEA, "Lat") & " Long: " & ParseGPS(sNMEA, "Long") & " Alt: " & ParseGPS(sNMEA, "Alt") & " Speed: " & ParseGPS(sNMEA, "Speed") & "MPH"
+                        SyncLock GPSClass.GPSData
+                            GPSClass.GPSData = sData
+                        End SyncLock
+                        'Console.WriteLine(sData)
+                    End If
+                Catch ex As Exception
+                    Console.WriteLine("GPS Read Failed.")
+                End Try
+            End If
+            System.Threading.Thread.Sleep(250)
+        Loop
+    End Sub
+
+    Private Sub ProcessGSM(ByVal Port As String)
+        Dim gsmPort As New SerialPort                            'New Serial Port: GSM.
+        Dim sGSM
+        Dim sGPSSend
+        Dim bPassword As Boolean
+
+        Try
+            With gsmPort
+                .PortName = "COM9" 'GSM port
+                .BaudRate = 9600
+                .Open()
+            End With
+
+            If gsmPort.IsOpen Then Console.WriteLine("GSM Online.")
+        Catch x As Exception
+            Console.WriteLine("GSM Failed. Could not access port. Check device and settings.")
+            End
+        End Try
+        'Console.WriteLine("Testing modem...")
+        gsmPort.WriteLine("AT" & vbCrLf)
+        'Console.WriteLine("Sent AT.")
+        Threading.Thread.Sleep(100)
+        gsmPort.WriteLine("AT+CMGF=1" & vbCrLf)
+        'Console.WriteLine("Switching SMS Mode...")
+        Threading.Thread.Sleep(100)
+        gsmPort.WriteLine("AT+CNMI=1,1,0,0,0" & vbCrLf)
+        Threading.Thread.Sleep(100)
+        gsmPort.WriteLine("AT+COPS=?" & vbCrLf)
+        Threading.Thread.Sleep(100)
+
+        Do
+            If gsmPort.BytesToRead > 0 And gsmPort.IsOpen = True Then
+                Try
+                    sGSM = gsmPort.ReadExisting 'Grab incoming data from GSM modem
+                    'Console.WriteLine(sGSM)
+                    sGSM = ParseGSM(sGSM, "SENDGPS")
+                    If InStr(sGSM, "SEND:") Then
+                        sGSM = Replace(sGSM, "SEND:", "")
+                        gsmPort.WriteLine(sGSM & vbCrLf)
+                        Console.WriteLine("Request Received, Processing...")
+                    End If
+                    If InStr(sGSM, "MSG:") Then
+                        bPassword = True
+                        sGSM = Replace(sGSM, "MSG:", "")
+                        gsmPort.WriteLine("AT+CMGS=" & Chr(34) & sGSM & Chr(34) & vbCrLf)
+                    End If
+                    If InStr(sGSM, "SENDMSG") Then
+                        If bPassword = True Then
+                            sGSM = Replace(sGSM, "SENDMSG", "")
+                            SyncLock GPSClass.GPSData
+                                sGPSSend = GPSClass.GPSData
+                            End SyncLock
+                            gsmPort.WriteLine("IcyEwe v1.0 Tracking System: " & sGPSSend & Chr(26))
+                            Console.WriteLine("Success.")
+                        Else
+                            sGSM = Replace(sGSM, "SENDMSG", "")
+                            sGPSSend = "You are not authorised to use this tracker. Request failed."
+                            gsmPort.WriteLine("IcyEwe v1.0 Tracking System: " & sGPSSend & Chr(26))
+                            Console.WriteLine("Bad Request.")
+                        End If
+
+                    End If
+                    If InStr(sGSM, "PWI:") Then
+                        bPassword = False
+                        sGSM = Replace(sGSM, "PWI:", "")
+                        gsmPort.WriteLine("AT+CMGS=" & Chr(34) & sGSM & Chr(34) & vbCrLf)
+                    End If
+                Catch ex As Exception
+                    Console.WriteLine("GSM Read Failed.")
+                End Try
+            End If
+            System.Threading.Thread.Sleep(100)
+        Loop
+
+    End Sub
+
+
+End Module
---
+++ track/Parser.vb
@@ -0,0 +1,323 @@
+﻿Module Parser
+
+    Dim gpsNextLine As String
+    Dim gpsNextField As String
+    Dim gpsNextChar As String
+    Dim gpsCheckChar As String
+
+    Dim gpsGGATime
+    Dim gpsGGALatitude
+    Dim gpsGGALatitudeNS
+    Dim gpsGGALongitude
+    Dim gpsGGALongitudeEW
+    Dim gpsGGAFixQuality
+    Dim gpsGGASatellites
+    Dim gpsGGAHorizontalDilution
+    Dim gpsGGAAltitude
+    Dim gpsGGAAltUnits
+    Dim gpsGGAGeoidHeight
+    Dim gpsGGASinceLastUpdate
+    Dim gpsGGADGPSStationID
+    Dim gpsGGAChecksum
+
+    Dim gpsGSADSelect
+    Dim gpsGSA2D3D
+    Dim gpsGSAPRN(11)
+    Dim gpsGSAPDOP
+    Dim gpsGSAHDOP
+    Dim gpsGSAVDOP
+    Dim gpsGSAChecksum
+
+    Dim gpsGSVSentences
+    Dim gpsGSVSentence
+    Dim gpsGSVPRN
+    Dim gpsGSVElevation
+    Dim gpsGSVAzimuth
+    Dim gpsGSVSNR(3)
+    Dim gpsGSVChecksum
+
+    Dim gpsVTGKMH
+    Dim gpsVTGMPH
+
+    Dim gpsDecLat
+    Dim gpsDecLong
+
+    Dim gpsGGA(16) As String
+    Dim gpsGSA(18) As String
+    Dim gpsGSV(21) As String
+    Dim gpsVTG(8) As String
+
+    Dim gpsGGASentence As String
+
+    Dim XORString As String
+    Dim XORSum As String
+    Dim XORChar As String
+
+    Public Const fixInvalid = 0
+    Public Const fixSPS = 1
+    Public Const fixDGPS = 2
+    Public Const fixPPS = 3
+    Public Const fixRTK = 4
+    Public Const fixFRTK = 5
+    Public Const fixEstimated = 6
+    Public Const fixManual = 7
+    Public Const fixSimulation = 8
+
+    Public Function gpsChecksum(ByVal XORString As String)
+        '//////////////////// Function to calculate GPS checksum
+        XORSum = "00"
+        XORChar = ""
+
+        For i = 0 To Len(XORString) - 1
+            XORSum = XORSum Xor Asc(Mid$(XORString, i + 1, 1))
+        Next
+
+        Return (Hex$(XORSum))
+    End Function
+
+    Public Function ParseGPS(ByVal Sentence As String, ByVal Request As String)
+        '//////////////////// GPS sentence parser
+        Dim sData As String
+        gpsNextLine = Sentence
+
+        '//////////////////// Parse Global Positioning System Fix Data (GGA)
+        If Strings.Left(gpsNextLine, 6) = "$GPGGA" Then
+            gpsGGA = Split(gpsNextLine, ",")
+            gpsGGATime = gpsGGA(1) : gpsGGATime = Val(gpsGGATime)
+            gpsGGALatitude = gpsGGA(2)
+            gpsGGALatitudeNS = gpsGGA(3)
+            gpsGGALongitude = gpsGGA(4)
+            gpsGGALongitudeEW = gpsGGA(5)
+            gpsGGAFixQuality = gpsGGA(6)
+            gpsGGASatellites = gpsGGA(7)
+            gpsGGAHorizontalDilution = gpsGGA(8)
+            gpsGGAAltitude = gpsGGA(9)
+            gpsGGAAltUnits = gpsGGA(10)
+            gpsGGAGeoidHeight = gpsGGA(11)
+            gpsGGAAltUnits = gpsGGA(12)
+            gpsGGASinceLastUpdate = gpsGGA(13)
+            gpsGGAChecksum = Strings.Right(gpsGGA(14), 3)
+
+            '//////////////////// Assemble GGA Sentence
+            gpsGGASentence = "GPGGA" & "," & gpsGGA(1) & "," & gpsGGA(2) & "," & gpsGGA(3) & "," & gpsGGA(4) & "," & gpsGGA(5) & "," & gpsGGA(6) & "," & gpsGGA(7) & "," & gpsGGA(8) & "," & gpsGGA(9) & "," & gpsGGA(10) & "," & gpsGGA(11) & "," & gpsGGA(12) & "," & gpsGGA(13) & ","
+
+            '//////////////////// Return Requested Data
+            Select Case Request
+                Case "Time"
+                    sData = Format(gpsGGATime, "00:00:00")
+                    sData = Replace(sData, vbCr, vbNullString)
+                    sData = Replace(sData, vbLf, vbNullString)
+                    sData = Replace(sData, vbCrLf, vbNullString)
+                    sData = Trim(sData)
+                    Return sData
+                Case "Long"
+                    sData = LongToDecimal(gpsGGALongitude) & gpsGGALongitudeEW
+                    sData = Replace(sData, vbCr, vbNullString)
+                    sData = Replace(sData, vbLf, vbNullString)
+                    sData = Replace(sData, vbCrLf, vbNullString)
+                    sData = Trim(sData)
+                    Return sData
+                Case "Lat"
+                    sData = LatToDecimal(gpsGGALatitude) & gpsGGALatitudeNS
+                    sData = Replace(sData, vbCr, vbNullString)
+                    sData = Replace(sData, vbLf, vbNullString)
+                    sData = Replace(sData, vbCrLf, vbNullString)
+                    sData = Trim(sData)
+                    Return sData
+                Case "Head"
+                    sData = Replace(sData, vbCr, vbNullString)
+                    sData = Replace(sData, vbLf, vbNullString)
+                    sData = Replace(sData, vbCrLf, vbNullString)
+                    sData = Trim(sData)
+                    Return sData
+                Case "Speed"
+                    sData = gpsVTGMPH
+                    sData = Replace(sData, vbCr, vbNullString)
+                    sData = Replace(sData, vbLf, vbNullString)
+                    sData = Replace(sData, vbCrLf, vbNullString)
+                    sData = Trim(sData)
+                    Return sData
+                Case "Alt"
+                    sData = gpsGGAAltitude & "m"
+                    sData = Replace(sData, vbCr, vbNullString)
+                    sData = Replace(sData, vbLf, vbNullString)
+                    sData = Replace(sData, vbCrLf, vbNullString)
+                    sData = Trim(sData)
+                    Return sData
+                Case "NMEA"
+                    sData = gpsGGALongitude & gpsGGALongitudeEW & " " & gpsGGALatitude & gpsGGALatitudeNS
+                    Return sData
+            End Select
+
+        End If
+
+        '//////////////////// Parse Satellite Status (GSA)
+        If Strings.Left(gpsNextLine, 6) = "$GPGSA" Then
+            gpsGSA = Split(gpsNextLine, ",")
+            gpsGSADSelect = gpsGSA(1)
+            gpsGSA2D3D = gpsGSA(2)
+            For gpsArrayCount = 0 To 11
+                gpsGSAPRN(gpsArrayCount) = gpsGSA(gpsArrayCount + 3)
+            Next
+            gpsGSAPDOP = gpsGSA(15)
+            gpsGSAHDOP = gpsGSA(16)
+            gpsGSAChecksum = Strings.Right(gpsGSA(17), 3)
+            gpsGSAVDOP = Strings.Left(gpsGSA(17), Len(gpsGSA(17)) - 4)
+        End If
+
+        '//////////////////// Parse Satellites In View (GSV)
+        If Strings.Left(gpsNextLine, 6) = "$GPGSV" Then
+            gpsGSV = Split(gpsNextLine, ",")
+
+        End If
+
+        '//////////////////// End Parse Velocity (VTG)
+        If Strings.Left(gpsNextLine, 6) = "$GPVTG" Then
+            gpsVTG = Split(gpsNextLine, ",")
+            gpsVTGKMH = gpsVTG(7)
+            gpsVTGMPH = Format(gpsVTGKMH / 1.609344, "##0.00")
+            Select Case Request
+                Case "Speed"
+                    sData = gpsVTGMPH
+                    sData = Replace(sData, vbCr, vbNullString)
+                    sData = Replace(sData, vbLf, vbNullString)
+                    sData = Replace(sData, vbCrLf, vbNullString)
+                    sData = Trim(sData)
+                    Return sData
+            End Select
+        End If
+    End Function
+
+    Public Function ParseGSM(ByVal Command As String, ByVal Password As String)
+        '//////////////////// GSM modem stats parser
+        If InStr(Command, "+CMTI: ", CompareMethod.Text) Then
+            Dim aSMSIncoming
+            Command = Replace(Command, Chr(34), vbNullString)
+            Command = Replace(Command, "+CMT: ", vbNullString)
+            aSMSIncoming = Split(Command, ",")
+            Return "SEND:AT+CMGR=" & aSMSIncoming(1) & vbCrLf
+        End If
+
+        If InStr(Command, "+CMGR: ", CompareMethod.Text) Then
+            Dim aSMSIncoming
+            Dim sDate
+            Dim sNumber
+            Dim sText
+
+            Command = Replace(Command, "+CMGR: ", vbNullString)
+            Command = Replace(Command, Chr(34), vbNullString)
+            Command = Replace(Command, Chr(10), vbNullString)
+            Command = Replace(Command, Chr(13), vbNullString)
+            sText = Strings.Right(Command, Len(Command) - InStrRev(Command, Chr(34)))
+            aSMSIncoming = Split(Command, ",")
+            sNumber = aSMSIncoming(1)
+            sText = Replace(sText, "OK", vbNullString)
+            sDate = aSMSIncoming(3)
+            If InStr(Command, Password) Then
+                Return "MSG:" & sNumber
+            Else
+                Return "PWI:" & sNumber
+            End If
+        End If
+
+        If InStr(Command, ">", CompareMethod.Text) Then
+            Return "SENDMSG"
+        End If
+
+        If InStr(Command, "+COPS: ", CompareMethod.Text) Then
+            Dim aOperators As Array
+            Dim aParams As Array
+            Command = Strings.Replace(Command, "+COPS: ", vbNullString)
+            Command = Strings.Replace(Command, "),", "|")
+            Command = Strings.Replace(Command, "(", vbNullString)
+            Command = Strings.Replace(Command, Chr(34), vbNullString)
+            aOperators = Split(Command, "|")
+            For o = 0 To UBound(aOperators) - 2
+                aParams = Split(aOperators(o), ",")
+                If Val(aParams(o)) = 2 Then
+                    Console.WriteLine("")
+                    Console.WriteLine("SMS Enabled, Powered by " & aParams(1) & ".")
+                    Console.WriteLine("")
+                End If
+            Next
+        End If
+    End Function
+
+    '////////////////////////////////////////////////////////////////////////////////////////////////////
+    '//////////////////// Decimal to NMEA/NMEA to Decimal conversion, from http://www.tma.dk/gps/
+    '////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    Public Enum enumLongLat
+        Latitude = 1
+        Longitude = 2
+    End Enum
+
+    Public Enum enumReturnformat
+        WithSigns = 0
+        NMEA = 1
+    End Enum
+
+    Private Function LongToDecimal(ByVal sLong As String) As Double
+        Dim dLon As Double = Convert.ToDouble(sLong)
+        dLon = dLon / 100
+        Dim lon() As String = dLon.ToString().Split(".")
+        Return lon(0).ToString() + "." + ((Convert.ToDouble(lon(1)) / 60)).ToString("#####")
+    End Function
+
+    Private Function LatToDecimal(ByVal sLat As String) As Double
+        Dim dLat As Double = Convert.ToDouble(sLat)
+        dLat = dLat / 100
+        Dim lat() As String = dLat.ToString().Split(".")
+        Return lat(0).ToString() + "." + ((Convert.ToDouble(lat(1)) / 60)).ToString("#####")
+    End Function
+
+    Public Function DecimalPosToDegrees(ByVal Decimalpos As Double, ByVal Type As enumLongLat, ByVal Outputformat As enumReturnformat, Optional ByVal SecondResolution As Integer = 2) As String
+        Dim Deg As Integer = 0
+        Dim Min As Double = 0
+        Dim Sec As Double = 0
+        Dim Dir As String = ""
+        Dim tmpPos As Double = Decimalpos
+        If tmpPos < 0 Then tmpPos = Decimalpos * -1 'Always do math on positive values
+
+        Deg = CType(Math.Floor(tmpPos), Integer)
+        Min = (tmpPos - Deg) * 60
+        Sec = (Min - Math.Floor(Min)) * 60
+        Min = Math.Floor(Min)
+        Sec = Math.Round(Sec, SecondResolution)
+
+        Select Case Type
+            Case enumLongLat.Latitude '=1
+                If Decimalpos < 0 Then
+                    Dir = "S"
+                Else
+                    Dir = "N"
+                End If
+            Case enumLongLat.Longitude '=2
+                If Decimalpos < 0 Then
+                    Dir = "W"
+                Else
+                    Dir = "E"
+                End If
+        End Select
+        Select Case Outputformat
+            Case enumReturnformat.NMEA
+                Return AddZeros(Deg, 3) & AddZeros(Min, 2) & AddZeros(Sec, 2)
+            Case enumReturnformat.WithSigns
+                Return Deg & "°" & Min & """" & Sec & "'" & Dir
+            Case Else
+                Return ""
+        End Select
+    End Function
+
+    Public Function AddZeros(ByVal Value As Double, ByVal Zeros As Integer) As String
+        If Math.Floor(Value).ToString.Length < Zeros Then
+            Return Value.ToString.PadLeft(Zeros, CType("0", Char))
+        Else
+            Return Value.ToString
+        End If
+    End Function
+
+    '////////////////////////////////////////////////////////////////////////////////////////////////////
+    '//////////////////// End of code from http://www.tma.dk/gps/
+    '////////////////////////////////////////////////////////////////////////////////////////////////////
+End Module
